aa7685a60158dc1cf68716460c4562098b15c465
Nick Dujay <nick.dujay@instacart.com>
2019-10-26 20:51:16 -0400

    add config as a volume map; git commit and almost push

--- docker/hledger-update/start.sh
+++ docker/hledger-update/start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-ls ~/ssh-keys
-
 # add the private key to the ssh agent
 eval $(ssh-agent -s)
 ssh-add ~/ssh-keys/id_rsa
@@ -11,9 +9,20 @@ ssh-keyscan github.com > ~/.ssh/known_hosts
 
 # clone repo
 git clone git@github.com:dat2/hledger-transactions.git
+cd hledger-transactions
 
-# TODO update the script
+# update the script, print yesterdays transactions, since
+# we'll be running at midnight every day
+YESTERDAY=$(date -d yesterday '+%Y/%m/%d')
+echo $YESTERDAY
+hledger-exporter export -s $YESTERDAY >> .hledger.journal
 
-# git add .
-# git commit -m "update by k8s"
+git diff
+
+# copy git config so we can commit
+cp /conf/.gitconfig ~/.gitconfig
+
+# commit and push
+git add .
+git commit -m "update: $YESTERDAY"
 # git push origin master
