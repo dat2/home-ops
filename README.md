d3b86b98b4429b08391066d67dc7e2158012b1a2
Nick Dujay <nick.dujay@instacart.com>
2019-11-15 15:10:14 -0500

    change from cron job to regular job

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is my repo of personal projects.
 
 ## hledger-update
 
-This uses [plaid](https://plaid.com/) to automatically update an [hledger](https://hledger.org/) journal nightly.
+This uses my [hledger-exporter](https://github.com/dat2/hledger-exporter) tool to update a `hledger` journal at midnight every day.
 
 Make sure to [generate an SSH key](), and create the secret:
 
@@ -15,3 +15,7 @@ How to run:
 - Build the docker image: `docker build docker/hledger-update --tag hledger-update`.
 - Create the cron job: `kubectl apply -f k8s`
 - Create the plaid secret: `kubectl create secret generic plaid-secrets --from-literal=client_id=<PLAID_CLIENT_ID> --from-literal=secret=<PLAID_SECRET>--from-literal=public_key=<PLAID_PUBLIC_KEY> --from-literal=access_token=<PLAID ACCESS TOKEN>`
+
+## hledger-web
+
+This uses `hledger-web` and `hledger-api` to show transactions.
--- docker/hledger-update/start.sh
+++ docker/hledger-update/start.sh
@@ -11,17 +11,19 @@ ssh-keyscan github.com > ~/.ssh/known_hosts
 git clone git@github.com:dat2/hledger-transactions.git
 cd hledger-transactions
 
-# update the script, print yesterdays transactions, since
-# we'll be running at midnight every day
-YESTERDAY=$(date -d 'yesterday' '+%Y/%m/%d')
-hledger-exporter $YESTERDAY >> .hledger.journal
+# use the last date reported from the journal +1 day.
+export LEDGER_FILE=$PWD/.hledger.journal
+LAST_DATE_IN_JOURNAL=$(hledger activity | tail -n 1 | cut -d' ' -f1 | xargs date '+%Y/%m/%d' -d)
+EXPORT_START_DATE=$(date -d "$LAST_DATE_IN_JOURNAL + 1 day" '+%Y/%m/%d')
+
+echo "updating from $EXPORT_START_DATE to $(date '+%Y/%m/%d')"
+hledger-exporter $EXPORT_START_DATE >> .hledger.journal
 
 # copy git config so we can commit
 cp /conf/.gitconfig ~/.gitconfig
 
 # commit and push
 git add .
-git commit -m "update: $YESTERDAY"
+git commit -m "update from $LAST_DATE_IN_JOURNAL to $EXPORT_START_DATE"
 
-# don't push until we're safe
 # git push origin master
